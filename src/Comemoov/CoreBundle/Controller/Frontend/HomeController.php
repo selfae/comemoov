1c00a3c7e43840911da120c351569b3aeaba0322
selfae <camille.hernoux@gmail.com>
2017-11-16 15:10:27 +0100

    [+] Ajout d'une page CMS sur la Home page et personalisation de la charte graphique

--- src/Comemoov/CoreBundle/Controller/Frontend/HomeController.php
+++ src/Comemoov/CoreBundle/Controller/Frontend/HomeController.php
@@ -15,6 +15,8 @@ use Cocorico\CoreBundle\Repository\ListingRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Cocorico\PageBundle\Repository\PageRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Class HomeController
@@ -22,5 +24,34 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class HomeController extends CocoricoHomeController
 {
+    /**
+     * @Route("/", name="cocorico_home")
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
 
+        /** @var PageRepository $page */
+        $page = $em->getRepository('CocoricoPageBundle:Page')->findOneBySlug(
+            'home',
+            $request->getLocale()
+        );
+        if (!$page) {
+            throw new NotFoundHttpException(sprintf('%s page not found.', 'home'));
+        }
+        /** @var ListingRepository $listingRepository */
+        $listingRepository = $this->getDoctrine()->getRepository('CocoricoCoreBundle:Listing');
+        $listings = $listingRepository->findByHighestRanking(6, $request->getLocale());
+
+        return $this->render(
+            'ComemoovCoreBundle:Frontend\Home:index.html.twig',
+            array(
+                'listings'  => $listings,
+                'page'      => $page,
+            )
+        );
+    }
 }
